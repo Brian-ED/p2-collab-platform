e733456a14ca64609f5680b79a83ca0532bb316a
Brian-ED <brianellingsgaard9@gmail.com>
2025-03-10 15:44:42 +0100

    Capped text also reverted page.tsx on this branch

--- src/app/components/boxed-project.jsx
+++ src/app/components/boxed-project.jsx
@@ -1,5 +1,16 @@
+function cap(text, max) {
+  if (max<text.length){
+    text = text.slice(0, max-3)+"..."
+  }
+  return text
+}
+
 export const BoxedProject = ({title="MISSING", memberNames="MISSING", startDate="MISSING", onClick}) => {
-  
+  const maxLineLength = 17;
+  memberNames = cap(memberNames, maxLineLength);
+  startDate   = cap(startDate  , maxLineLength);
+  title       = cap(title      , maxLineLength);
+
   return (
     <button onClick={onClick} className="py-5 rounded-md bg-blue-500 hover:bg-blue-600 size-32">
       <div>{title}</div>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,16 +1,5 @@
 import { TestComponent } from "@/app/components/test";
-import { BoxedProject } from "@/app/components/boxed-project";
 
 export default function Home() {
-  return (
-    <div className="items-center space-y-4 space-x-10">
-      <div></div>
-      <BoxedProject title="Title1" startDate=""/>
-      <BoxedProject title="Title2" startDate=""/>
-      <BoxedProject />
-      <BoxedProject />
-      <BoxedProject />
-      <BoxedProject />
-    </div>
-  );
+  return <TestComponent />;
 }
