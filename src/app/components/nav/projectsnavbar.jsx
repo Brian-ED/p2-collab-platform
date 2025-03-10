f02b1307efe2be4f8d3fd290571d6335ec3d65bc
xahen <18694387+xahen@users.noreply.github.com>
2025-03-10 14:31:57 +0100

    initial setup

--- src/app/components/nav/projectsnavbar.jsx
+++ src/app/components/nav/projectsnavbar.jsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { useState } from "react";
+
 const NavButtons = [
     {
         name: "Overview",
@@ -34,7 +38,7 @@ const Button = ({ name, link, logo, section }) => {
     );
 };
 
-export const ProjectsNavbar = ({ setSection }) => {
+export const ProjectsNavbarOld = ({ setSection }) => {
     return (
         <nav>
             <div className="bg-[#00ADB5] w-screen h-12 flex flex-row">
@@ -48,4 +52,48 @@ export const ProjectsNavbar = ({ setSection }) => {
             </div>
         </nav>
     );
+};
+
+export const ProjectsNavbar = () => {
+  const [section, setSection] = useState("overview");
+  return (
+    <nav>
+      <div className="bg-[#00ADB5] w-screen h-12 flex flex-row">
+        <div>
+          <button
+            className="border-[#222831] text-black border-2 px-2 h-full hover:bg-white/40"
+            onClick={() => setSection("overview")}
+          >
+            Overview
+          </button>
+          <button
+            className="border-[#222831] text-black border-2 px-2 h-full hover:bg-white/40"
+            onClick={() => setSection("gantt")}
+          >
+            Gantt
+          </button>
+          <button
+            className="border-[#222831] text-black border-2 px-2 h-full hover:bg-white/40"
+            onClick={() => setSection("trello")}
+          >
+            Trello
+          </button>
+          <button
+            className="border-[#222831] text-black border-2 px-2 h-full hover:bg-white/40"
+            onClick={() => setSection("calendar")}
+          >
+            Calendar
+          </button>
+        </div>
+
+        <div></div>
+      </div>
+      <div>
+        {section === "overview" && <h1>overview</h1>}
+        {section === "gantt" && <h1>gantt</h1>}
+        {section === "trello" && <h1>trello</h1>}
+        {section === "calendar" && <h1>calendar</h1>}
+      </div>
+    </nav>
+  );
 };
