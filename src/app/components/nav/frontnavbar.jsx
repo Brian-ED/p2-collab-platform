5fcc904086d33396bfb8b4b3179a78c39f0e285b
xahen <18694387+xahen@users.noreply.github.com>
2025-03-11 15:46:13 +0100

    added transition animation

--- src/app/components/nav/frontnavbar.jsx
+++ src/app/components/nav/frontnavbar.jsx
@@ -27,39 +27,39 @@ import { useState } from "react";
 // the dropdown menu is rendered in the exported function component, this function component is rendered on the page via projects/page.jsx
 // see it on localhost:3000/projects/whatever
 export const FrontNavbar = () => {
-  return <DropdownMenu />;
+    return <DropdownMenu />;
 };
 
 // i have created the drop down menu in a separate function component, to make it easier for you to alter it
 // also, i intentionally made it very ugly, and only made slight CSS changes, so it's up to you guys to make it nicer
 const DropdownMenu = () => {
-  // here the 'expanded' state is created with the 'setExpanded' function to alter it
-  const [expanded, setExpanded] = useState(false);
+    // here the 'expanded' state is created with the 'setExpanded' function to alter it
+    const [expanded, setExpanded] = useState(false);
 
-  return (
-    <div>
-      {/* this button changes the expanded useState between 'true' and 'false' */}
-      <button
-        className="border-2 border-red-800 px-2 text-black bg-white hover:bg-white/80"
-        onClick={() => {
-          expanded ? setExpanded(false) : setExpanded(true);
-        }}
-      >
-        Menu
-      </button>
-      {/* this div is conditionally rendered based on the state of 'expanded' (this is our actual dropdown part) */}
-      <div
-        className={
-          (expanded ? "scale-100" : "scale-0") +
-          " bg-white text-black border-red-800 border-2 w-36"
-        }
-      >
-        <p className="hover:underline">test</p>
-        <p className="hover:underline">test 2</p>
-        <p className="hover:underline">test 3</p>
-        <p className="hover:underline">test 4</p>
-        <p className="hover:underline">test 5</p>
-      </div>
-    </div>
-  );
+    return (
+        <div>
+            {/* this button changes the expanded useState between 'true' and 'false' */}
+            <button
+                className="border-2 border-red-800 px-2 text-black bg-white hover:bg-white/80"
+                onClick={() => {
+                    expanded ? setExpanded(false) : setExpanded(true);
+                }}
+            >
+                Menu
+            </button>
+            {/* this div is conditionally rendered based on the state of 'expanded' (this is our actual dropdown part) */}
+            <div
+                className={
+                    (expanded ? "scale-100" : "scale-0") +
+                    " bg-white text-black border-red-800 border-2 w-36 transition-all duration-400"
+                }
+            >
+                <p className="hover:underline">test</p>
+                <p className="hover:underline">test 2</p>
+                <p className="hover:underline">test 3</p>
+                <p className="hover:underline">test 4</p>
+                <p className="hover:underline">test 5</p>
+            </div>
+        </div>
+    );
 };
