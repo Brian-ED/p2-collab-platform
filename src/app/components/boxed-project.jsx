f406f43f4b854b684df39a25b5275b699e9e0446
Brian-ED <brianellingsgaard9@gmail.com>
2025-03-14 12:30:07 +0100

    de-duplicated ellipse css

--- src/app/components/boxed-project.jsx
+++ src/app/components/boxed-project.jsx
@@ -1,8 +1,9 @@
 export const BoxedProject = ({title="MISSING", memberNames="MISSING", startDate="MISSING", onClick}) => {
+  const ellipses = "whitespace-nowrap overflow-hidden overflow-ellipsis"
   return (
     <button onClick={onClick} className="py-5 rounded-md bg-blue-500 hover:bg-blue-600 size-32">
-      <div className="leading-10 whitespace-nowrap overflow-hidden overflow-ellipsis">{title}</div>
-      <div className="text-[12px] leading-7 whitespace-nowrap overflow-hidden overflow-ellipsis">{memberNames}</div>
+      <div className={`${ellipses} leading-10`}>{title}</div>
+      <div className={`${ellipses} text-[12px] leading-7`}>{memberNames}</div>
       <div className="text-[13.5px]/9 italic">{startDate}</div>
     </button>
   );
