784f70dbc867f4d8e52e5ae9c15c5faff674d470
Brian-ED <brianellingsgaard9@gmail.com>
2025-03-14 12:18:29 +0100

    Implemented proper ellipses and styled the date

--- src/app/components/boxed-project.jsx
+++ src/app/components/boxed-project.jsx
@@ -1,21 +1,10 @@
-function cap(text, max) {
-  if (max<text.length){
-    text = text.slice(0, max-3)+"..."
-  }
-  return text
-}
-
 export const BoxedProject = ({title="MISSING", memberNames="MISSING", startDate="MISSING", onClick}) => {
   const maxLineLength = 17;
-  memberNames = cap(memberNames, maxLineLength);
-  startDate   = cap(startDate  , maxLineLength);
-  title       = cap(title      , maxLineLength);
-
   return (
     <button onClick={onClick} className="py-5 rounded-md bg-blue-500 hover:bg-blue-600 size-32">
-      <div>{title}</div>
-      <div>{memberNames}</div>
-      <div>{startDate}</div>
+      <div className="leading-10 whitespace-nowrap overflow-hidden overflow-ellipsis">{title}</div>
+      <div className="text-[12px] leading-7 whitespace-nowrap overflow-hidden overflow-ellipsis">{memberNames}</div>
+      <div className="text-[13.5px]/9 italic">{startDate}</div>
     </button>
   );
 };
