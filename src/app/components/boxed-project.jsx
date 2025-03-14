09963dc2b4785b7ee5fbca434cee45f671299c76
Brian-ED <brianellingsgaard9@gmail.com>
2025-03-14 12:33:50 +0100

    improved colors

--- src/app/components/boxed-project.jsx
+++ src/app/components/boxed-project.jsx
@@ -1,7 +1,7 @@
 export const BoxedProject = ({title="MISSING", memberNames="MISSING", startDate="MISSING", onClick}) => {
   const ellipses = "whitespace-nowrap overflow-hidden overflow-ellipsis"
   return (
-    <button onClick={onClick} className="py-5 rounded-md bg-blue-500 hover:bg-blue-600 size-32">
+    <button onClick={onClick} className="py-5 rounded-md bg-blue-600 hover:bg-blue-500 size-32">
       <div className={`${ellipses} leading-10`}>{title}</div>
       <div className={`${ellipses} text-[12px] leading-7`}>{memberNames}</div>
       <div className="text-[13.5px]/9 italic">{startDate}</div>
