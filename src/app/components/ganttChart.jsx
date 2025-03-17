8096ec4657c6191dadf4188f8ec986cb7db85e36
Oliver Dalgaard <oliver@ihd.dk>
2025-03-17 14:52:29 +0100

    Fat one glorp
    Co-Authored-By: oliver borg <18694387+xahen@users.noreply.github.com>

--- src/app/components/ganttChart.jsx
+++ src/app/components/ganttChart.jsx
@@ -1,14 +1,15 @@
 "use client";
 
+import { useState } from "react";
+
 const data = [
   {
     id: 1,
     title: "Test1",
     description: "This is test1",
-    startDate: new Date("17-03-2025"),
-    endDate: new Date("18-03-2025"),
+    startDate: new Date("2025-03-17"),
+    endDate: new Date("2025-03-18"),
     percentComplete: 50,
-    dependency: null,
   },
   {
     id: 2,
@@ -17,24 +18,81 @@ const data = [
     startDate: null,
     endDate: null,
     percentComplete: 25,
-    dependency: { id: 1, length: 5 },
   },
   {
     id: 3,
-    title: "Test3",
+    title: "Test3 yabba dabba doo shit on my shoe",
     description: "This is test3",
     startDate: null,
     endDate: null,
     percentComplete: 0,
-    dependency: { id: 2, length: 3 },
+  },
+  {
+    id: 4,
+    title: "Test4",
+    description: "This is test4",
+    startDate: null,
+    endDate: null,
+    percentComplete: 0,
+  },
+  {
+    id: 5,
+    title: "Test5",
+    description:
+      "Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor",
+    startDate: null,
+    endDate: null,
+    percentComplete: 0,
   },
 ];
+
+const GanttTask = ({
+  id,
+  title,
+  description,
+  startDate,
+  endDate,
+  percentComplete,
+}) => {
+  const [hover, setHover] = useState(false);
+
+  return (
+    <div className="h-12 w-screen flex">
+      <div
+        className="rounded-lg bg-taskcolor h-8 m-auto ml-0 flex group/task w-150"
+        onMouseEnter={() => {
+          setHover(true);
+        }}
+        onMouseLeave={() => {
+          setHover(false);
+        }}
+      >
+        <p className="m-auto ml-1 overflow-hidden group-hover/task:overflow-visible whitespace-nowrap">
+          {title}
+        </p>
+        <div
+          className={`absolute bg-white mt-10 ml-5 rounded-sm max-w-100 max-h-50 overflow-y-hidden ${
+            hover ? "scale-100" : "scale-0"
+          }`}
+        >
+          <p className="text-black p-2 m-auto">{description}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export const GanttChart = () => {
   return (
     <div>
-      <div className="bg-trackcolor h-12 w-screen"></div>
-      <div className="bg-trackcolorodd h-12 w-screen"></div>
+      <div className="[&>*:nth-child(odd)]:bg-trackcolorodd [&>*:nth-child(even)]:bg-trackcolor">
+        {data.map((task) => (
+          <GanttTask key={task.id} {...task} />
+        ))}
+      </div>
+      <div className="bg-amber-300 h-8 flex">
+        <p className="text-black m-auto ml-2">fat one glorp</p>
+      </div>
     </div>
   );
 };
