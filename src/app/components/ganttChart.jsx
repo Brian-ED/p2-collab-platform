ee7d1e1b73f9dc79f4274d6191287c8465e16c14
Oliver Dalgaard <oliver@ihd.dk>
2025-03-18 11:06:58 +0100

    Added length of tasks to the GanttTask function. Also added start- and enddate to task description

--- src/app/components/ganttChart.jsx
+++ src/app/components/ganttChart.jsx
@@ -15,24 +15,24 @@ const data = [
     id: 2,
     title: "Test2",
     description: "This is test2",
-    startDate: null,
-    endDate: null,
+    startDate: new Date("2025-03-19"),
+    endDate: new Date("2025-03-20"),
     percentComplete: 25,
   },
   {
     id: 3,
     title: "Test3 yabba dabba doo shit on my shoe",
     description: "This is test3",
-    startDate: null,
-    endDate: null,
+    startDate: new Date("2025-03-20"),
+    endDate: new Date("2025-03-21"),
     percentComplete: 0,
   },
   {
     id: 4,
     title: "Test4",
     description: "This is test4",
-    startDate: null,
-    endDate: null,
+    startDate: new Date("2025-03-22"),
+    endDate: new Date("2025-03-23"),
     percentComplete: 0,
   },
   {
@@ -40,8 +40,8 @@ const data = [
     title: "Test5",
     description:
       "Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor Lorem ipsum din mor",
-    startDate: null,
-    endDate: null,
+    startDate: new Date("2025-03-24"),
+    endDate: new Date("2025-03-30"),
     percentComplete: 0,
   },
 ];
@@ -55,11 +55,12 @@ const GanttTask = ({
   percentComplete,
 }) => {
   const [hover, setHover] = useState(false);
+  const taskDuration = (endDate - startDate) / (1000 * 60 * 60 * 24);
 
   return (
     <div className="h-12 w-screen flex">
       <div
-        className="rounded-lg bg-taskcolor h-8 m-auto ml-0 flex group/task w-150"
+        className={`rounded-lg bg-taskcolor h-8 m-auto ml-0 flex group/task w-150`}
         onMouseEnter={() => {
           setHover(true);
         }}
@@ -75,7 +76,8 @@ const GanttTask = ({
             hover ? "scale-100" : "scale-0"
           }`}
         >
-          <p className="text-black p-2 m-auto">{description}</p>
+          <p className="text-black p-2 m-auto pb-1">{`${startDate.getDate()}/${startDate.getMonth()} - ${endDate.getDate()}/${endDate.getMonth()}`}</p>
+          <p className="text-black p-2 m-auto pt-0">{description}</p>
         </div>
       </div>
     </div>
@@ -90,8 +92,11 @@ export const GanttChart = () => {
           <GanttTask key={task.id} {...task} />
         ))}
       </div>
-      <div className="bg-amber-300 h-8 flex">
-        <p className="text-black m-auto ml-2">fat one glorp</p>
+      <div className="bg-amber-300 h-8 flex pl-2">
+        {}
+        <p className="text-black m-auto ml-0 mr-5">
+          {data[0].startDate.getDate()}
+        </p>
       </div>
     </div>
   );
