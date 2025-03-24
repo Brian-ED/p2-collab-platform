82126729674b4f7916d1f3a833a9be40296e307f
Brian-ED <brianellingsgaard9@gmail.com>
2025-03-24 10:07:52 +0100

    let→const, a?b:c → if(a){b}else{c}

--- src/components/loginButton.jsx
+++ src/components/loginButton.jsx
@@ -1,29 +1,33 @@
 import { signIn, signOut, auth } from "@/auth/authSetup";
 
 export const LoginButton = async () => {
-  let session = await auth();
+  const session = await auth();
+  let signInOrOutButton;
 
-  return (
-    <div>
-      {session?.user ? (
-        <form
-          action={async () => {
-            "use server";
-            await signOut();
-          }}
-        >
-          <button type="submit">Logout</button>
-        </form>
-      ) : (
-        <form
-          action={async () => {
-            "use server";
-            await signIn("github");
-          }}
-        >
-          <button type="submit">Login with GitHub</button>
-        </form>
-      )}
-    </div>
-  );
+  // Select page depending on if user is logged in
+  if (session?.user) { // Logged in
+    signInOrOutButton = (
+      <form
+        action={async () => {
+          "use server";
+          await signOut();
+        }}
+      >
+        <button type="submit">Logout</button>
+      </form>
+    )
+  } else { // Logged Out
+    signInOrOutButton = (
+      <form
+        action={async () => {
+          "use server";
+          await signIn("github");
+        }}
+      >
+        <button type="submit">Login with GitHub</button>
+      </form>
+    )
+  }
+
+  return signInOrOutButton
 };
