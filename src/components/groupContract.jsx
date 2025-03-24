d98e7db1b890768e40fc49718cf35dbf4fb46bb5
kresten-j <kads24@student.aau.dk>
2025-03-24 14:03:56 +0100

    working on group contract

---
+++ src/components/groupContract.jsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { useState } from "react";
+
+const GroupContract = () => {
+  return (
+    <div className="container bg-black">
+      <div>test</div>
+    </div>
+  );
+};
+
+export default GroupContract;
