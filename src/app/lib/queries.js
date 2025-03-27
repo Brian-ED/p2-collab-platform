125970b7f0862b6aa0b7d2fa3ceb1e81d28f6bd9
Oliver Dalgaard <oliver@ihd.dk>
2025-03-27 15:04:43 +0100

    Users are now added to the database

---
+++ src/app/lib/queries.js
@@ -0,0 +1,15 @@
+import pg from "pg";
+
+const { Pool } = pg;
+
+const pool = new Pool({
+  connectionTimeoutMillis: 10000,
+  connectionString: process.env.DATABASE_URL,
+  allowExitOnIdle: false,
+});
+
+export async function addUser(name, userId) {
+  await pool.query(
+    `INSERT INTO users (name, user_id) VALUES ('${name}', '${userId}') ON CONFLICT (user_id) DO NOTHING;`
+  );
+}
--- src/auth/authSetup.js
+++ src/auth/authSetup.js
@@ -9,7 +9,6 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   callbacks: {
     authorized: async ({ auth }) => {
       // Logged in users are authenticated, otherwise redirect to login page
-      console.log(auth);
       return !!auth;
     },
   },
