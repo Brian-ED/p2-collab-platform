8d7f4ee48c5754ae42092bceee713b48c6c0ed73
Oliver Dalgaard <oliver@ihd.dk>
2025-03-27 13:11:19 +0100

    Added user_id to user table

--- src/app/lib/queries.js
+++ src/app/lib/queries.js
@@ -5,3 +5,7 @@ const sql = postgres(process.env.DATABASE_URL);
 export async function checkIfTablesExist() {
   await sql.file("./src/app/lib/checktables.sql");
 }
+
+export async function addUser(name, userId) {
+  await sql`INSERT INTO users (name, userId) VALUES (${name}, ${userId})`;
+}
