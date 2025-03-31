5144877aadfb6bf00232f8200edcba58f50fb0ea
Oliver Dalgaard <oliver@ihd.dk>
2025-03-31 13:17:12 +0200

    User can now access not owned projects
    Users can now access different projects, that they are added on

--- src/app/projects/[pid]/page.jsx
+++ src/app/projects/[pid]/page.jsx
@@ -19,7 +19,7 @@ export default function Projects() {
     fetch(`/api/db/ownsProject?projectId=${pid}`)
       .then((res) => res.json())
       .then((data) => {
-        if (!data.userOwnsProject) redirect("/");
+        if (!data.userOwnsProject && !data.userHasAccess) redirect("/");
         setIsLoading(false);
       });
   }, []);
