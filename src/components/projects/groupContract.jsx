e5f73c30cbbc9ae0f2795fcd15109b33815d4d28
kresten-j <kads24@student.aau.dk>
2025-04-01 10:41:53 +0200

    save changes

--- src/components/projects/groupContract.jsx
+++ src/components/projects/groupContract.jsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { FaPlus, FaTrash } from "react-icons/fa";
 import { useState } from "react";
 
 export const GroupContract = () => {
@@ -36,7 +37,7 @@ export const GroupContract = () => {
       ...prevRules,
       {
         id: Date.now(),
-        title: "New category",
+        title: newRuleInputs[categoryId],
         rules: [],
       },
     ]);
@@ -89,7 +90,24 @@ export const GroupContract = () => {
   return (
     <div className="p-4">
       <h2 className="text-4xl font-bold mb-4">Group Contract</h2>
-      <button onClick={() => addCategory()}>Add category</button>
+      <button
+        onClick={() => addCategory()}
+        className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      >
+        <input
+          type="text"
+          placeholder={`Enter new category`}
+          value={newRuleInputs[category.id] || ""}
+          onChange={(e) => handleInputChange(category.id, e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              submitOnEnter(e, category.id);
+            }
+          }}
+          className="border p-2 rounded w-[30%] mr-2 transition-all h-10 duration-300 focus:h-25 resize-none"
+        />
+        Add category
+      </button>
       {contractRules.map((category) => (
         <div key={category.id} className="">
           <div className="py-4">
@@ -101,7 +119,7 @@ export const GroupContract = () => {
                 </li>
               ))}
             </ul>
-            <div className="mt-2 flex">
+            <div className="mt-2 flex items-start">
               <textarea
                 type="text"
                 placeholder={`Enter new rule for ${category.title.toLowerCase()}`}
@@ -114,7 +132,14 @@ export const GroupContract = () => {
                 }}
                 className="border p-2 rounded w-[30%] mr-2 transition-all h-10 duration-300 focus:h-25 resize-none"
               />
-              <button onClick={() => addRule(category.id)}>Add Rule</button>
+              <button
+                onClick={() => {
+                  addRule(category.id);
+                }}
+                className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 font-medium rounded-full text-sm px-2.5 py-2.5 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 flex-shrink-0]>"
+              >
+                <FaPlus className="text-sm" />
+              </button>
             </div>
           </div>
         </div>
