02644b3708d10c9f034f215e248ab4d382649bea
Oliver Dalgaard <oliver@ihd.dk>
2025-04-02 11:19:54 +0200

    Api endpoints for group contract

---
+++ src/app/api/db/editItemFromGroupContract/route.js
@@ -0,0 +1,37 @@
+import { auth } from "@/auth/authSetup";
+
+export async function DELETE(req) {
+  const session = await auth();
+
+  console.log(req);
+
+  if (!!session) {
+    return Response.json({ data: "DELETE HANDLED", error: null });
+  } else {
+    return Response.json({ data: null, error: "Not authorized" });
+  }
+}
+
+export async function PATCH(req) {
+  const session = await auth();
+
+  console.log(req);
+
+  if (!!session) {
+    return Response.json({ data: "PATCH HANDLED", error: null });
+  } else {
+    return Response.json({ data: null, error: "Not authorized" });
+  }
+}
+
+export async function POST(req) {
+  const session = await auth();
+
+  console.log(req);
+
+  if (!!session) {
+    return Response.json({ data: "POST HANDLED", error: null });
+  } else {
+    return Response.json({ data: null, error: "Not authorized" });
+  }
+}
