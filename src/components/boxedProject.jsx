ccf64ce22391c240df2d41a956d4fa78b03c7ad1
Brian-ED <brianellingsgaard9@gmail.com>
2025-04-02 14:19:32 +0200

    renamed component

--- src/components/boxedProject.jsx
+++ src/components/boxedProject.jsx
@@ -6,18 +6,18 @@ export const BoxedProject = ({
   onClick,
 }) => {
   const ellipses = "whitespace-nowrap overflow-hidden overflow-ellipsis";
+  const names = memberNames.map((member) => (
+    <div key={member.id} className={`${ellipses} text-[12px] leading-7`}>
+      {member.name}
+    </div>
+  ));
   return (
     <button
       onClick={onClick}
       className="py-5 rounded-md bg-blue-600 hover:bg-blue-500 hover:cursor-pointer size-50"
     >
       <div className={`${ellipses} leading-10`}>{title}</div>
-      {memberNames.map((member) => (
-        <div key={member.id} className={`${ellipses} text-[12px] leading-7`}>
-          {member.name}
-        </div>
-      ))}
-      {/* <div className="text-[13.5px]/9 italic">{startDate}</div> */}
+      {names}
     </button>
   );
 };
