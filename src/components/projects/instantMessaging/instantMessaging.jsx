c5557dee381b7498dfc5e81e10919d97b372971d
Melihkav <mkavak24@student.aau.dk>
2025-04-09 14:18:22 +0200

    Socket update

--- src/components/projects/instantMessaging/instantMessaging.jsx
+++ src/components/projects/instantMessaging/instantMessaging.jsx
@@ -1,3 +1,23 @@
+import { useEffect } from "react";
+
 export const InstantMessaging = () => {
+  useEffect(() => {
+    const ws = new WebSocket("ws://localhost:3000/api/socket");
+    
+    ws.onopen = () => {
+      console.log("Connected to WebSocket");
+      ws.send("Hello, WebSocket!");
+    };
+    ws.onmessage = (event) => {
+      console.log("Message received:", event.data);
+    };
+    ws.onclose = () => {
+      console.log("WebSocket connection closed");
+    };
+    return () => {
+      ws.close();
+    };
+  }, []);
+
   return <div></div>;
 };
