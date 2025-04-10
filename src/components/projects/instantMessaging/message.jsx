a991d29f6db8a2d502c381e0e9933e38c3234729
Melihkav <mkavak24@student.aau.dk>
2025-04-10 15:44:47 +0200

    Development of chat-system
    Co-Authored-By: Aloharapeco <180258060+Aloharapeco@users.noreply.github.com>

--- src/components/projects/instantMessaging/message.jsx
+++ src/components/projects/instantMessaging/message.jsx
@@ -1,6 +1,6 @@
 export const Message = ({ message, sender, time }) => {
   return (
-    <div className="flex gap-5">
+    <div className="flex flex-row gap-4 p-4 max-w-md bg-white shadow-md rounded-2xl border border-gray-200 text-black mb-3">
       <p>{sender}</p>
       <h1>{message}</h1>
       <p>{time}</p>
