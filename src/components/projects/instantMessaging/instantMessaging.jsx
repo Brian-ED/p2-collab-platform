2d8792c783e332b809d2c2780d37ad1966a930f0
Oliver Dalgaard <oliver@ihd.dk>
2025-04-10 15:19:22 +0200

    Users can now send messages

--- src/components/projects/instantMessaging/instantMessaging.jsx
+++ src/components/projects/instantMessaging/instantMessaging.jsx
@@ -3,6 +3,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
 import { Message } from "@/components/projects/instantMessaging/message";
+import { IoSend } from "react-icons/io5";
 
 export const InstantMessaging = () => {
   const { pid } = useParams();
@@ -16,9 +17,23 @@ export const InstantMessaging = () => {
     };
   }, []);
 
+  const sendMessage = () => {
+    const data = new URLSearchParams(
+      new FormData(document.querySelector("#message"))
+    );
+    fetch(`/api/chat?projectId=${pid}`, {
+      method: "POST",
+      body: data,
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    }).then((response) => {
+      console.log(response);
+    });
+  };
+
   return (
     <div>
-      <p>Message</p>
       {messages.map((message) => (
         <Message
           key={message.id}
@@ -27,6 +42,23 @@ export const InstantMessaging = () => {
           time={message.time_sent}
         />
       ))}
+      <form
+        action={() => {
+          sendMessage();
+        }}
+        className="flex w-[calc(100%-var(--spacing)*68)] rounded-3xl bg-button fixed bottom-5"
+        id="message"
+      >
+        <input
+          type="text"
+          name="message"
+          maxLength={255}
+          className="w-[96%] mx-auto outline-hidden"
+        />
+        <button type="submit" className="hover:cursor-pointer outline-hidden">
+          <IoSend className="mx-2" />
+        </button>
+      </form>
     </div>
   );
 };
