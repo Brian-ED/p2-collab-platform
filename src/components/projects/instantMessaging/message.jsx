7dd13dfc4001d797463d8165e8da1aaee108a26e
Melihkav <mkavak24@student.aau.dk>
2025-04-15 10:42:59 +0200

    FInal instant message

--- src/components/projects/instantMessaging/message.jsx
+++ src/components/projects/instantMessaging/message.jsx
@@ -1,9 +1,16 @@
+import dayjs from "dayjs";
+
 export const Message = ({ message, sender, time }) => {
+  const currentDate = dayjs(time);
+  const formattedDate = currentDate.format('YYYY-MM-DD kl. HH:mm');
+  
   return (
-    <div className="flex flex-row gap-4 p-4 max-w-md bg-white shadow-md rounded-2xl border border-gray-200 text-black mb-3">
-      <p>{sender}</p>
-      <h1>{message}</h1>
-      <p>{time}</p>
+    <div className="flex flex-row gap-4 p-4 max-w-2xl bg-white shadow-md rounded-2xl border border-gray-200 mb-3 w-fit">
+      <div className="flex flex-col w-full break-words">
+        <p className="font-semibold text-black">{sender}</p>
+        <p className="text-base text-black whitespace-pre-wrap">{message}</p>
+        <p className="text-xs text-gray-400 mt-2">{formattedDate}</p>
+      </div>
     </div>
   );
 };
