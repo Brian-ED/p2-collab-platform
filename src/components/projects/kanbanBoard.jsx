44f9dd96e3bf37092f60404ef6f35c5952e40abb
xahen <18694387+xahen@users.noreply.github.com>
2025-04-15 11:02:53 +0200

    set up test data + entry mapping

--- src/components/projects/kanbanBoard.jsx
+++ src/components/projects/kanbanBoard.jsx
@@ -1,9 +1,54 @@
+const entries = [
+  {
+    name: "test 1",
+    section: "backlog",
+    users: [],
+  },
+  {
+    name: "test 2",
+    section: "backlog",
+    users: [],
+  },
+  {
+    name: "test 3",
+    section: "progress",
+    users: [],
+  },
+  {
+    name: "test 4",
+    section: "review",
+    users: [],
+  },
+  {
+    name: "test 5",
+    section: "review",
+    users: [],
+  },
+  {
+    name: "test 6",
+    section: "done",
+    users: [],
+  },
+];
+
+const KanbanEntry = ({ name, section, users }) => {
+  return (
+    <div className="h-fit w-full">
+      <h3 className="text-lg">{name}</h3>
+    </div>
+  );
+};
+
 export const KanbanBoard = () => {
   return (
     <div className="flex flex-row">
       <section className="m-2 w-full h-fit border-3 rounded-3xl">
         <h1 className="text-xl text-center my-2">Backlog</h1>
-        <div></div>
+        <div>
+          {entries.map((entry) => (
+            <KanbanEntry key={entry.name} {...entry} />
+          ))}
+        </div>
       </section>
       <section className="m-2 w-full border-3 rounded-3xl">
         <h1 className="text-xl text-center my-2">In progress</h1>
