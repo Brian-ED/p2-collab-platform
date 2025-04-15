ddc1d279bf3dbae65e95d4192d1ed17e0611783f
xahen <18694387+xahen@users.noreply.github.com>
2025-04-15 10:49:33 +0200

    initialized sections

--- src/components/projects/kanbanBoard.jsx
+++ src/components/projects/kanbanBoard.jsx
@@ -1,7 +1,22 @@
 export const KanbanBoard = () => {
   return (
-    <>
-      <h1>Kanban Board</h1>
-    </>
+    <div className="flex flex-row">
+      <section className="m-2 w-full h-fit border-3 rounded-3xl">
+        <h1 className="text-xl text-center my-2">Backlog</h1>
+        <div></div>
+      </section>
+      <section className="m-2 w-full border-3 rounded-3xl">
+        <h1 className="text-xl text-center my-2">In progress</h1>
+        <div></div>
+      </section>
+      <section className="m-2 w-full border-3 rounded-3xl">
+        <h1 className="text-xl text-center my-2">In review</h1>
+        <div></div>
+      </section>
+      <section className="m-2 w-full border-3 rounded-3xl">
+        <h1 className="text-xl text-center my-2">Done</h1>
+        <div></div>
+      </section>
+    </div>
   );
 };
