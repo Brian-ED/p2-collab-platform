ea62c73a31d3850ef30876b733bd189e7b582314
kresten-j <kads24@student.aau.dk>
2025-04-15 11:09:34 +0200

    test

--- src/app/api/db/handleItemInGroupContract/route.js
+++ src/app/api/db/handleItemInGroupContract/route.js
@@ -1,11 +1,26 @@
 import { auth } from "@/auth/authSetup";
+import {
+  getGroupContract,
+  checkIfUserOwnsProject,
+  checkIfUserHasAccessToProject,
+} from "@/lib/queries";
 
 export async function GET() {
+  let projectId = new URL(req.url).searchParams.get("projectId");
+  projectId = parseInt(projectId);
+
   const session = await auth();
 
-  if (!!session) {
-    let projectId = new URL(req.url).searchParams.get("projectId");
-    projectId = parseInt(projectId);
+  let userHasAccess = false;
+  if (Number.isInteger(projectId)) {
+    userHasAccess =
+      (await checkIfUserOwnsProject(session, projectId)) ||
+      (await checkIfUserHasAccessToProject(session, projectId));
+  }
+
+  if (!!session && userHasAccess) {
+    const data = await getGroupContract(projectId);
+    return Response.json({ data: data, error: null });
   } else {
     return Response.json({ data: null, error: "Not authorized" });
   }
