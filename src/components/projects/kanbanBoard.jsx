6a5b7398d799fe3738bd382de0d876d8ba6ecefa
xahen <18694387+xahen@users.noreply.github.com>
2025-04-15 11:09:23 +0200

    conditionally rendered entries based on given section

--- src/components/projects/kanbanBoard.jsx
+++ src/components/projects/kanbanBoard.jsx
@@ -1,31 +1,51 @@
 const entries = [
   {
-    name: "test 1",
+    name: "backlog test 1",
     section: "backlog",
     users: [],
   },
   {
-    name: "test 2",
+    name: "backlog test 2",
     section: "backlog",
     users: [],
   },
   {
-    name: "test 3",
+    name: "progress test 1",
     section: "progress",
     users: [],
   },
   {
-    name: "test 4",
+    name: "progress test 2",
+    section: "progress",
+    users: [],
+  },
+  {
+    name: "progress test 3",
+    section: "progress",
+    users: [],
+  },
+  {
+    name: "review test 1",
     section: "review",
     users: [],
   },
   {
-    name: "test 5",
+    name: "review test 2",
     section: "review",
     users: [],
   },
   {
-    name: "test 6",
+    name: "done test 1",
+    section: "done",
+    users: [],
+  },
+  {
+    name: "done test 2",
+    section: "done",
+    users: [],
+  },
+  {
+    name: "done test 3",
     section: "done",
     users: [],
   },
@@ -33,7 +53,7 @@ const entries = [
 
 const KanbanEntry = ({ name, section, users }) => {
   return (
-    <div className="h-fit w-full">
+    <div className="h-fit w-full p-2">
       <h3 className="text-lg">{name}</h3>
     </div>
   );
@@ -45,22 +65,46 @@ export const KanbanBoard = () => {
       <section className="m-2 w-full h-fit border-3 rounded-3xl">
         <h1 className="text-xl text-center my-2">Backlog</h1>
         <div>
-          {entries.map((entry) => (
-            <KanbanEntry key={entry.name} {...entry} />
-          ))}
+          {entries.map(
+            (entry) =>
+              entry.section === "backlog" && (
+                <KanbanEntry key={entry.name} {...entry} />
+              )
+          )}
         </div>
       </section>
       <section className="m-2 w-full border-3 rounded-3xl">
         <h1 className="text-xl text-center my-2">In progress</h1>
-        <div></div>
+        <div>
+          {entries.map(
+            (entry) =>
+              entry.section === "progress" && (
+                <KanbanEntry key={entry.name} {...entry} />
+              )
+          )}
+        </div>
       </section>
       <section className="m-2 w-full border-3 rounded-3xl">
         <h1 className="text-xl text-center my-2">In review</h1>
-        <div></div>
+        <div>
+          {entries.map(
+            (entry) =>
+              entry.section === "review" && (
+                <KanbanEntry key={entry.name} {...entry} />
+              )
+          )}
+        </div>
       </section>
       <section className="m-2 w-full border-3 rounded-3xl">
         <h1 className="text-xl text-center my-2">Done</h1>
-        <div></div>
+        <div>
+          {entries.map(
+            (entry) =>
+              entry.section === "done" && (
+                <KanbanEntry key={entry.name} {...entry} />
+              )
+          )}
+        </div>
       </section>
     </div>
   );
