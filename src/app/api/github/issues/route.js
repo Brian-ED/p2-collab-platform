32eafd155080fc76bf6654c85203b3387ed3bb48
Oliver Dalgaard <oliver@ihd.dk>
2025-04-23 14:37:12 +0200

    Github API endpoints setup

---
+++ src/app/api/github/issues/route.js
@@ -0,0 +1,44 @@
+// pages/api/github/issues.ts
+import { App } from "octokit";
+
+export async function GET(req) {
+  try {
+    const app = new App({
+      appId: Number(process.env.APP_ID),
+      privateKey: process.env.APP_PRIVATE_KEY,
+      oauth: {
+        clientId: process.env.AUTH_GITHUB_ID,
+        clientSecret: process.env.AUTH_GITHUB_SECRET,
+      },
+    });
+
+    const octokit = await app.getInstallationOctokit(65132728);
+
+    const response = await octokit.request(
+      "GET /repos/Brian-ED/p2-collab-platform/issues",
+      {
+        owner: "Brian-ED",
+        repo: "p2-collab-platform",
+        headers: {
+          "X-GitHub-Api-Version": "2022-11-28",
+        },
+      }
+    );
+
+    const response2 = await octokit.request(
+      "GET /repos/Brian-ED/p2-collab-platform/issues/comments",
+      {
+        owner: "Brian-ED",
+        repo: "p2-collab-platform",
+        headers: {
+          "X-GitHub-Api-Version": "2022-11-28",
+        },
+      }
+    );
+
+    return Response.json(response.data[0].body);
+  } catch (error) {
+    console.error("GitHub API Error:", error);
+    return Response.json({ error: "GitHub API error" });
+  }
+}
