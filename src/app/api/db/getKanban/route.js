1cfd947a01f47463c96c22a432f1fdfbe9894b19
xahen <18694387+xahen@users.noreply.github.com>
2025-04-25 11:08:01 +0200

    initialized api endpoint

---
+++ src/app/api/db/getKanban/route.js
@@ -0,0 +1,25 @@
+import { auth } from "@/auth/authSetup";
+
+import {
+  checkIfUserOwnsProject,
+  checkIfUserHasAccessToProject,
+} from "@/lib/queries";
+
+export async function GET(req) {
+  let projectId = new URL(req.url).searchParams.get("projectId");
+  projectId = parseInt(projectId);
+
+  const session = await auth();
+  let userHasAccess = false;
+  if (Number.isInteger(projectId)) {
+    userHasAccess = await checkIfUserOwnsProject(session, projectId) ||
+                    await checkIfUserHasAccessToProject(session, projectId);
+  }
+
+  if (!!session && userHasAccess) {
+    //const data = await getGanttTasks(projectId);
+    return Response.json({ data: data, error: null });
+  } else {
+    return Response.json({ data: null, error: "Not authorized" });
+  }
+}
