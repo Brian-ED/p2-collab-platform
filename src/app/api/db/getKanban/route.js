ddd79ae022c459c06b1439518f00a130ddd9b22f
xahen <18694387+xahen@users.noreply.github.com>
2025-04-25 11:12:41 +0200

    initialized query

--- src/app/api/db/getKanban/route.js
+++ src/app/api/db/getKanban/route.js
@@ -3,6 +3,7 @@ import { auth } from "@/auth/authSetup";
 import {
   checkIfUserOwnsProject,
   checkIfUserHasAccessToProject,
+  getKanbanEntries,
 } from "@/lib/queries";
 
 export async function GET(req) {
@@ -12,12 +13,13 @@ export async function GET(req) {
   const session = await auth();
   let userHasAccess = false;
   if (Number.isInteger(projectId)) {
-    userHasAccess = await checkIfUserOwnsProject(session, projectId) ||
-                    await checkIfUserHasAccessToProject(session, projectId);
+    userHasAccess =
+      (await checkIfUserOwnsProject(session, projectId)) ||
+      (await checkIfUserHasAccessToProject(session, projectId));
   }
 
   if (!!session && userHasAccess) {
-    //const data = await getGanttTasks(projectId);
+    const data = await getKanbanEntries(projectId);
     return Response.json({ data: data, error: null });
   } else {
     return Response.json({ data: null, error: "Not authorized" });
