dcf7af816566b92cb50724a7ff22a8c27f6c8d77
kresten-j <kads24@student.aau.dk>
2025-05-21 22:42:06 +0200

    changed font type to Roboto - a lot smoother font compared to the default :)))

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,14 +1,17 @@
 import "./globals.css";
 import { AppProvider } from "@/context/AppContext";
 
-export const metadata = {
-  title: "P2 Collab Platform",
-  description: "",
-};
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <title>P2 Collab Platform</title>
+        <meta name="description" content="Collaborative project platform" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body>
         <AppProvider>{children}</AppProvider>
       </body>
